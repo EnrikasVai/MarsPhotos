feb1d6916387587380a8be42c13de12ec8050fe3
Enrikas Vaiciulis <mac@Enrikass-MacBook-Pro.local>
2023-10-14 16:18:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/marsphotos/ui/MarsPhotosApp.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.marsphotos.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.marsphotos.R
+import com.example.marsphotos.ui.screens.HomeScreen
+import com.example.marsphotos.ui.screens.MarsViewModel
+
+@Composable
+fun MarsPhotosApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val (isAuthorInfoVisible, setAuthorInfoVisible) = remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            MarsTopAppBar(scrollBehavior = scrollBehavior) {
+                setAuthorInfoVisible(true)
+            }
+        }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            val marsViewModel: MarsViewModel =
+                viewModel(factory = MarsViewModel.Factory)
+            HomeScreen(
+                marsUiState = marsViewModel.marsUiState,
+                retryAction = marsViewModel::getMarsPhotos
+            )
+        }
+    }
+
+    // Show the author information dialog when isAuthorInfoVisible is true
+    if (isAuthorInfoVisible) {
+        AuthorInfoDialog(
+            onDismiss = { setAuthorInfoVisible(false) }
+        )
+    }
+}
+
+@Composable
+fun AuthorInfoDialog(onDismiss: () -> Unit) {
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp), // Adjust the corner radius as needed
+            //backgroundColor = Color.White
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(20.dp)
+            ) {
+                Text("Autorius: Enrikas Vaiciulis")
+                Text("Grupe: MKDF 20/4")
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { onDismiss() }
+                ) {
+                    Text("OK")
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun MarsTopAppBar(
+    scrollBehavior: TopAppBarScrollBehavior,
+    modifier: Modifier = Modifier,
+    onAuthorClick: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        actions = {
+            // Add the "Author" menu item with an onClick event handler
+            IconButton(
+                onClick = { onAuthorClick() }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Person, // You can replace this with an appropriate icon
+                    contentDescription = stringResource(R.string.author)
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
